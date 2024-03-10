1d0e050f9dd659ba58818a1258cb33ab16afb63e
Vachan MN <vachanmn123@gmail.com>
2024-03-10 19:27:31 +0530

    Refactor page components and add PageBase component

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
 import HomePage from "./routes/home";
 import AboutPage from "./routes/about";
+import PageBase from "./components/PageBase";
 
 function App() {
   const location = useLocation();
   return (
     <>
-      <h1>Header</h1>
       <AnimatePresence>
+        <h1>Header</h1>
         <Routes location={location} key={location.pathname}>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
         </Routes>
+        <p>Footer</p>
       </AnimatePresence>
-      <p>Footer</p>
     </>
   );
 }
--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,16 +1,12 @@
-import { motion } from "framer-motion";
+import PageBase from "@/components/PageBase";
 import { Link } from "react-router-dom";
 
 export default function AboutPage() {
   return (
-    <motion.div
-      initial={{ y: -20, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 0.7, ease: [0.6, -0.05, 0.01, 0.99] }}
-    >
+    <PageBase>
       <h1>About Page</h1>
       <div className="w-[25rem] h-[25rem] bg-red-500"></div>
       <Link to="/">Go to Home</Link>
-    </motion.div>
+    </PageBase>
   );
 }
